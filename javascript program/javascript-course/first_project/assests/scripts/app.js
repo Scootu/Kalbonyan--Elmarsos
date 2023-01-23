ff3281bab6752b42290b08188b444a9131348fc2
scootu <bacetd2004@gmail.com>
2023-01-23 13:10:57 -0500

    creat javascript ĉourse

---
+++ javascript program/javascript-course/first_project/assests/scripts/app.js
@@ -0,0 +1,7 @@
+
+let defaultResult = 0 ;
+let currentResult = defaultResult ; 
+
+let currentResultDescription = `(${currentResult} +10/2+3)` ;
+currentResult = defaultResult +10/2 +3 ; 
+outputResult(currentResultDescription,currentResult); 
