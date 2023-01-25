2272d2c95522b03756065a6dd2a4bc033b668cb7
scootu <bacetd2004@gmail.com>
2023-01-25 14:41:06 -0500

    section 1 Unconveinait calculator work with basic buil-in javascript statement

---
+++ javascript program/javascript-course/console-input-continue-consept.js
@@ -0,0 +1,14 @@
+let i = 0 ; 
+let n = 0 ; 
+let arr = [] ;
+while (i<10) { 
+  i++;
+  if(i === 3) { 
+    continue ; // this mean don't excute the code  
+  }
+  n+=i ; 
+ // console.log(n);
+
+  arr.push(n);
+  
+}
--- javascript program/javascript-course/first_project/assests/scripts/app.js
+++ javascript program/javascript-course/first_project/assests/scripts/app.js
@@ -2,6 +2,46 @@
 let defaultResult = 0 ;
 let currentResult = defaultResult ; 
 
+function getUserInput() { 
+    return parseInt(userInput.value) ; 
+}
+
+function creatAndWriteOutput(operator ,resultBeforCaLL,callNum) { 
+         const calculDescription = `${resultBeforCaLL} ${operator} ${callNum} `;
+         outputResult(calculDescription,currentResult) ; // we use currentResult here (global value)
+}
+
+function add(num1 , num2) { 
+ const entredNumber = getUserInput() ; 
+ const initialNumber = currentResult ; 
+ currentResult = currentResult + entredNumber ;
+ creatAndWriteOutput('+',initialNumber,entredNumber);
+}
+
+function subtract() {
+    const entredNumber = getUserInput();
+    const initialNumber = currentResult ; 
+    currentResult = currentResult - entredNumber ; 
+    creatAndWriteOutput('-',initialNumber,entredNumber);
+ }
+
+function multiply() {
+    const entredNumber = getUserInput();
+    const initialNumber = currentResult ; 
+    currentResult = currentResult * entredNumber ; 
+    creatAndWriteOutput('*',initialNumber,entredNumber);
+}
+
+function divide() {
+    const entredNumber = getUserInput();
+    const initialNumber = currentResult ; 
+    currentResult = currentResult / entredNumber ; 
+    creatAndWriteOutput('/',initialNumber,entredNumber);
+ }
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
+
 let currentResultDescription = `(${currentResult} +10/2+3)` ;
-currentResult = defaultResult +10/2 +3 ; 
-outputResult(currentResultDescription,currentResult); 
+//outputResult(currentResultDescription,currentResult);  we change this because we want the output return value when we click in the button 
