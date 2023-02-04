70c225be5082251b032b60921300b4b503cd5657
scootu <bacetd2004@gmail.com>
2023-02-04 09:52:39 -0500

    finich the project 2

--- javascript program/javascript-course/second-project/asserts/scripts/app.js
+++ javascript program/javascript-course/second-project/asserts/scripts/app.js
@@ -4,58 +4,74 @@ function randomValue(max, min) {
     return Math.floor(Value);
   }
 
-let playerHealth =  getUserInput();
-//try ... catch statement 
+let playerHealth;
+
 let health = randomValue(15, 5);
 let playerAttack = randomValue(10,5);  
 let SpeacialAttack = playerAttack * 2;   
 let monsterHealth = 100 ; 
 let monsterAttack = randomValue(20,10);
-let monsterSpecialAttack = randomValue(30,20);
+const  defaultMonsterAttack  = randomValue(20,10); 
+let monsterSpecialAttack = randomValue(30,20) ;
+const  defaultMonsterSpecialAttack = randomValue(30,20) ; //
 let logEntries = [] ; 
 
 let countAttack =  0 ;
+//try ..catch 
+try { 
+    playerHealth = getUserInput() ; 
 
+    }catch(e) {
+     alert('default value is 100'); 
+     playerHealth = 100 ;
+     console.log('default value :true');  
+     }
+ //limiting value 
+   
+  limitingGameValue(playerHealth) ; 
 //output the result 
-userOutput(playerHealth,monsterHealth) ; 
+getResult(playerHealth,monsterHealth);
 //log 
 function writeToLog(
     caracter,
     countAttack ,
-    health,
-    isDraw ,
-    isWin ,
-
+    health
     ) { 
            const logEntry = {
               event:caracter,
               Attack: countAttack ,  
-              currentHealthValue:health,
-              Draw:isDraw,
-              Win:isWin
+              currentHealthValue:health
                  }
-        logEntries.push(logEntry);
-         console.log(logEntry);
+         logEntries.push(logEntry);
+         //console.log(logEntry);
+        // for (let i =0 ;i < logEntries.length ;i++) {
+              let i = countAttack ; 
+              console.log(`event : ${logEntries[i].event}`);
+              console.log(`Attack: ${logEntries[i].Attack}`);
+              console.log(`currentHealthValue: ${logEntries[i].currentHealthValue}`); 
+         //    }
     
     } ;
     
     //button 
 
 function attackHandler() { 
-  
+         if(playerHealth >=0 ) { 
          monsterHealth -=playerAttack ; 
-         writeToLog('Player Attack', countAttack ,playerHealth,isDraw(),false); 
-
+         }
+        
+         if(monsterHealth >=0) { 
          playerHealth -=monsterAttack ; 
+         }
          //writeToLog('Monster Attack', countAttack +,monsterHealth,isDraw(),isWin());
-
-         countAttack ++; 
-         
-         userOutput(playerHealth,monsterHealth);
+        // console.log(`Monster attack attackHandler ${monsterAttack}`);    
+         writeToLog('Player Attack', countAttack ,playerHealth); 
+         countAttack++; 
+         gameCheck(playerHealth,monsterHealth);
 }
 function strongAttackHundler() { 
          
-         if(monsterHealth === (2*playerHealth)) { 
+         if(monsterHealth >= (30 * playerHealth / 100)) { 
             monsterHealth -= SpeacialAttack ; 
             playerHealth -= monsterAttack ;  
          }else { 
@@ -63,39 +79,59 @@ function strongAttackHundler() {
             playerHealth -= monsterSpecialAttack ; 
             
          }
-          countAttack ++ ; 
-         writeToLog('Player Special Attack', countAttack ,playerHealth,isDraw());
-
-         userOutput(playerHealth , monsterHealth,) ; 
+         
+         writeToLog('Player Special Attack', countAttack ,playerHealth);
+     
+         countAttack++ ; 
+         gameCheck(playerHealth , monsterHealth,) ; 
 
 }
-let Round = 5 ; // هذا عجز  
+let Round = 4 ; // هذا عجز  
+let i = 1 ; 
 function playerHealthBounes() {
           //undefiend 
 
-         if(Round > 0) { 
-         
-         console.log('player use health!');
-         playerHealth += health;  
-         heal.innerHTML = (`heal ${Round}`); 
-         Round-- ; 
-
-         }else { 
-        
-            playerHealth -= monsterSpecialAttack ;
-            
-            console.log('respect the rool');
-         }
-      
+         if(Round >= 0) { 
        
-       userOutput(playerHealth,monsterHealth);
+           if(playerHealth <= monsterHealth * 0.1  ) 
+               playerHealth += 2 * health; 
+              else { 
+               playerHealth += health ; 
+               }    
+           console.log(`player use health! +${health} Times :${i++}`);
+           heal.innerHTML = (`heal ${Round--}`); 
+          
+      }else if(isWin(playerHealth,monsterHealth) === false ) { 
+             Round = 4 ; 
+      }else { 
+            
+            playerHealth -= monsterSpecialAttack ;
+            console.log(`respect the rool -${monsterSpecialAttack} Times :${i++}`);
+         }
+       
+       gameCheck(playerHealth,monsterHealth);
 
 }
 function playerSurrender() { 
-         confirm('you lose, you want to REPEAT !') ;    
+         getResult(0,monsterHealth) ; 
+         reste();
+         
+}
+function logEveryThing(){ 
+         //another way to do this ? 
+         console.log("this is log for every thing in the game") ;
+         console.log("---------------------------------------");
+         console.log("Monster ------------------ Slayer ") ; 
+         console.log("---------------------------------------");
+         for(let i =0;i<logEntries.length;i++) { 
+              
+              console.log(`event : ${logEntries[i].event}`);
+              console.log(`Attack: ${logEntries[i].Attack}`);
+              console.log(`currentHealthValue: ${logEntries[i].currentHealthValue}`); 
+         }
 }
 attack.addEventListener('click',attackHandler);
 heal.addEventListener('click',playerHealthBounes);
 special.addEventListener('click',strongAttackHundler);
-surrender.addEventListener('click',);
-
+surrender.addEventListener('click',playerSurrender);
+BattleLog.addEventListener('click',logEveryThing);
--- javascript program/javascript-course/second-project/asserts/scripts/vendor.js
+++ javascript program/javascript-course/second-project/asserts/scripts/vendor.js
@@ -1,54 +1,136 @@
 let monsterResult = document.getElementById('Monster');
 let playerResult = document.getElementById('player');
+let widthValueMonster = document.getElementById('barMonster'); // for width value 
+let widthValuePlayer = document.getElementById('barPlayer');
 let countRound = 0 ; 
 const attack = document.getElementById('attack');
 const heal = document.getElementById('heal');
 const special= document.getElementById('special-attack');
 const surrender = document.getElementById('surrender'); 
+const BattleLog = document.getElementById('BattleLog');
+function getResult(playerHealth,monsterHealth) { 
+         monsterResult.innerHTML = `Monster health:${monsterHealth}%`;
+         playerResult.innerHTML =`Player health : ${playerHealth}%`;
+         }
+         
+function streamWidhtPlayer(health) {
+        
+         if(health > 100) {
+            //virtual value 
+            
+            let x =( 100 / health ); 
+            health *= x ; //it's always 100% 
+           console.log(`the health value > 1000 ${health}` ); 
+           widthValuePlayer.style.width = `${health}%`;   
+           
+         }else {
+             widthValuePlayer.style.width = `${health}%` ;
+             
+         }         
+}
+function streamWidhtMonster(health) {
+             widthValueMonster.style.width = `${health}%` ;       
+}
 
-function userOutput(playerHealth,monsterHealth) { 
+function gameCheck(playerHealth,monsterHealth) { 
       
       if(isWin(playerHealth,monsterHealth) === undefined ) {  
-         monsterResult.innerHTML = `Monster health :${monsterHealth}%`; 
-         playerResult.innerHTML =`Player health : ${playerHealth}%`;
+               getResult(playerHealth,monsterHealth);
+               
+                streamWidhtMonster(monsterHealth);
+                streamWidhtPlayer(playerHealth) ; 
+               
       }else if(isWin(playerHealth,monsterHealth) === true) {
                countRound++ ;  
-               monsterResult.innerHTML = `Monster health :${0}%`; 
+               getResult(playerHealth,0) ;
+               //monsterResult.innerHTML = `Monster health :${0}%`; 
                alert('you win !');
-               if(confirm('do you want to repete ?')) { 
-                reste();
-            }
+               
+               reste();        
       }
       else if(isWin(playerHealth,monsterHealth) === false) { 
-        playerHealth = 0 ;
-        playerResult.innerHTML = `Player health : ${playerHealth}` ;
+       //local scoope
+        
+        
+        //playerResult.innerHTML = `Player health : ${playerHealth}` ;
         countRound++ ; 
         alert("you lose !"); 
+        getResult(0,monsterHealth);
+        streamWidhtMonster(monsterHealth);
+        streamWidhtPlayer(0) ; 
         isBouns(countRound);
-           
-
+        
          
       }else if(isDraw()) {
+        getResult(0,0);
+        /*
         monsterResult.innerHTML = `Monster health :${0}%`; 
         playerResult.innerHTML =`Player health : ${0}%`;
+        */
         countRound++;
         alert('is a draw !');    
-         
+          streamWidhtMonster(monsterHealth);
+       streamWidhtPlayer(playerHealth) ; 
          
         
        }
-    
+       /*
+       streamWidhtMonster(monsterHealth);
+       streamWidhtPlayer(playerHealth) ; 
+       */
 }
 
+
+   
 function getUserInput() { 
-    return  parseInt(prompt('what the health of the player'));
-}
+        let userInputValue =  parseInt(prompt('what the health of the player')) ;
+        if(isNaN(userInputValue)) { 
+             alert('Number Invalide') ;
+             throw 'invalide number'  ;
+             //reste();
+             }else if(isNaN(userInputValue) === false) {
+                     return userInputValue ; 
+             }
+             //max value i want to control in each throw and build cathe base on it 
+       
+ }
+ 
+function limitingGameValue(userInputValue) {
+          //default value 
+            
+       if(userInputValue >= 100) {
+          monsterAttack = monsterAttack * (userInputValue * 0.01) ; 
+          monsterSpecialAttack  *=(userInputValue * 0.01) ;
+         }  
+         if(userInputValue === 25 ) { 
+            monsterAttack = defaultMonsterAttack;
+            monsterSpecialAttack = defaultMonsterSpecialAttack ; 
+            }
+         if(userInputValue >= 1000) { 
+            alert('maximem value is 1000') ;
+            playerHealth = 1000 ; 
+            }
+           
+             
+   }  
 function reste() { 
+
     if(confirm('do you want to repete ?')) { 
          playerHealth = getUserInput();
          monsterHealth = 100 ; 
          console.log('repete game'); 
-        }
+         getResult(playerHealth,monsterHealth);
+         heal.innerHTML = `heal ${5}` ; 
+         
+         //limiting value  
+         limitingGameValue(playerHealth) ; 
+         //console.log(`Monster attack reste ${monsterAttack}`);  
+        }else if(isWin(playerHealth,monsterHealth))  { 
+         getResult(playerHealth,0);//are this enafe 
+        }else if(isWin(playerHealth,monsterHealth)) {
+         getResult(0,monsterHealth); 
+        }else {}
+        return true ; 
 }
 function isDraw() { 
     if(playerHealth <=0 && monsterHealth <=0) { 
@@ -85,7 +167,16 @@ function isBouns(countRound) {
          if(countRound === 1) { 
             alert('bounce live !') ; 
             playerHealth = 25 ;
-            playerResult.innerHTML = `Player health : ${playerHealth}`;     
+          //  streamWidhtPlayer(playerHealth) ; 
+            getResult(playerHealth,monsterHealth);
+            limitingGameValue(playerHealth) ; 
+           
+            streamWidhtPlayer(25) ; 
+            writeToLog('Bouns points', countAttack ,playerHealth);
+            
+            //console.log(`Monster attack isBouns ${monsterAttack}`);  
+            //playerResult.innerHTML = `Player health : ${playerHealth}`;
+            
          }else { 
          reste();
          }
