13825aed85e494e827304a9237c229ff94a9d115
scootu <bacetd2004@gmail.com>
2023-02-17 18:29:35 -0500

    Working with note app 'deleting and add a notes

--- javascript program/javascript-course/note app/assests/scripts/vendor.js
+++ javascript program/javascript-course/note app/assests/scripts/vendor.js
@@ -1,12 +1,15 @@
 const addBox = document.querySelector('.add-box'),
 popupBox = document.querySelector(".popup-box"),
 closeIcon = popupBox.querySelector('header i'),
-titleTag = popupBox.querySelector('input'),
-descTag = popupBox.querySelector('textarea');
-addBtn = popupBox.querySelector('button');
+titleTag = popupBox.querySelector('.input'),
+descTag = popupBox.querySelector('.textarea'),
+addBtn = popupBox.querySelector('button'),
+settingbtn = document.body.querySelector('.settingBtn'),
+iconSetting = document.body.querySelector('.uil.uil-ellipsis-h');
 
 addBox.addEventListener("click",()=> { 
    popupBox.classList.add('show');
+  
 });
 
 closeIcon.addEventListener("click",()=> { 
@@ -14,5 +17,16 @@ closeIcon.addEventListener("click",()=> {
  });
 
  addBtn.addEventListener('click',()=> { 
-    console.log('Add a Note !');
+   console.log(`input :${titleTag.value} , description : ${descTag.value}`);
+ });
+
+ iconSetting.addEventListener('click',()=> {
+   /*
+   timerId = setTimeout(()=> {
+      settingbtn.classList.add('show');
+      clearTimeout(timerId);
+   },300); 
+   */
+   settingbtn.classList.remove('show');
+   console.log(`${iconSetting}`);
  });
