135ddf475392721f8606740a29d06d7dd37d895f
scoote <bacetd2004@gmail.com>
2023-03-18 07:37:35 -1100

    working with project

--- javascript program/javascript-course/kalbonainalmarsor the thired exam/script.js
+++ javascript program/javascript-course/kalbonainalmarsor the thired exam/script.js
@@ -1,24 +1,24 @@
 //const taskList = document.querySelectorAll('')
-const task = document.querySelectorAll('task');
-const startList = document.querySelector('.task-container.start'),
-    progList = document.querySelector('.task-container.prog'),
-    compList = document.querySelector('.task-container.comp');
+const task = document.getElementsByClassName("task");
+const startList = document.querySelector(".task-container.start"),
+  progList = document.querySelector(".task-container.prog"),
+  compList = document.querySelector(".task-container.comp");
 
 //btn
-const startListBtn = startList.querySelector('.addBtn'),
-    progListBtn = progList.querySelector('.addBtn'),
-    compListBtn = compList.querySelector('.addBtn');
+const startListBtn = startList.querySelector(".addBtn"),
+  progListBtn = progList.querySelector(".addBtn"),
+  compListBtn = compList.querySelector(".addBtn");
 
-// standared list 
+// standared list
 let userListtasks = [];
 
-//create task list 
+//create task list
 
 function createList() {
-    //   console.log(this.previousElementSibling) 
-    let indexElem = indexElementFun();
-    let indexContent;
-    let liTag = `
+  //   console.log(this.previousElementSibling)
+  let indexElem = indexElementFun();
+  let indexContent;
+  let liTag = `
   <li class="task" draggable="true" data-index=${indexElem} >
   <input type="text" class="taskInput" placeholder ="click in any point to finich" >
   <span class="control">
@@ -30,65 +30,75 @@ function createList() {
       </span>
   </span>
 </li>
-  ` ;
-    // log in Object 
+  `;
+  // log in Object
 
-    let elemObjarr = [];
+  let elemObjarr = [];
 
+  this.previousElementSibling.insertAdjacentHTML("beforeend", liTag);
+  let taskListItems = this.previousElementSibling.querySelectorAll(".task");
 
-    this.previousElementSibling.insertAdjacentHTML('beforeend', liTag);
-    let taskListItems = this.previousElementSibling.querySelectorAll('.task');
+  taskListItems.forEach((elem) => {
+    let inputTag = elem.firstElementChild;
+    indexContent = inputTag.value;
 
+    //object element
 
-
-    taskListItems.forEach((elem) => {
-
-
-        let inputTag = elem.firstElementChild;
-        indexContent = inputTag.value;
-
-        //object element 
-
-
-        inputTag.addEventListener('keydown', (ekey) => {
-            if (ekey.key == '') {
-                inputTag.disabled = false;
-            }
-            else if (ekey.key == "Enter") {
-                inputTag.disabled = true;
-            }
-        });
-
-        let elemEvent = elem.querySelector('.control').firstElementChild;
-        elemEvent.addEventListener('click', (event) => {
-            inputTag.disabled = false;
-        });
-        // save to a local storage 1 copy  
+    inputTag.addEventListener("keydown", (ekey) => {
+      if (ekey.key == "") {
+        inputTag.disabled = false;
+      } else if (ekey.key == "Enter") {
+        inputTag.disabled = true;
+         // log in array
         
-        let elemObj = {
-            index: indexElem,
-            content: indexContent,
-        };
-        // log in array 
-        elemObjarr.push(elemObj);
-        localStorage.setItem("tasks", JSON.stringify(elemObjarr));
-
+      }
     });
-       userListtasks = JSON.parse(localStorage.getItem('tasks'));
-       console.log(userListtasks);
+
+    let elemEvent = elem.querySelector(".control").firstElementChild;
+    elemEvent.addEventListener("click", (event) => {
+      inputTag.disabled = false;
+    });
+    // save to a local storage 1 copy
+   
+    let elemObj = {
+      index: indexElem,
+      content: indexContent,
+    };
+    
+     
+  });
+ 
 }
-// 
+ userListtasks = JSON.parse(localStorage.getItem("tasks"));
+  console.log(userListtasks);
+//
 function indexElementFun() {
-    return Math.random();
+  return Math.random();
+}
+//Update Tasks  
+
+
+function UpdateTasks(arrObj,itemElem) {
+    task.forEach((elem)=>{
+     console.log(elem);
+    });
+    arrObj.push(itemElem);
+    localStorage.setItem("tasks", JSON.stringify(arrObj));
 }
 
 
-//Event listener btn 
+//Event listener btn
+task.forEach((elem) =>{
+    elem.addEventListener('keydown',(key)=> {
+      if(key.key == "Enter") {
+            inputTag.disabled = true;
+         } 
+})
+ }) ; 
+ 
 
-startListBtn.addEventListener('click', createList);
-progListBtn.addEventListener('click', createList);
-compListBtn.addEventListener('click', createList);
+startListBtn.addEventListener("click", createList);
+progListBtn.addEventListener("click", createList);
+compListBtn.addEventListener("click", createList);
 
 //
-
-
