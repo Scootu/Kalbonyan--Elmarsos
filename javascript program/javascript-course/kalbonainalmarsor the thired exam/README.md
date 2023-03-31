e672a0533e6371ca9c05be16485c53a809dbc845
scootu <bacetd2004@gmail.com>
2023-03-31 11:58:20 -0400

    edit readmi file

--- javascript program/javascript-course/kalbonainalmarsor the thired exam/README.md
+++ javascript program/javascript-course/kalbonainalmarsor the thired exam/README.md
@@ -14,5 +14,5 @@
 
 ### this project is dynamic you can do multible operation in one time 
 
- #### For next update i will make this project more dynamic 
- **save , edit ,delet and drag more than one task**
+ #### For next update i will make this project for mobile device 
+ 
--- javascript program/javascript-course/kalbonainalmarsor the thired exam/script/script.js
+++ javascript program/javascript-course/kalbonainalmarsor the thired exam/script/script.js
@@ -1,3 +1,6 @@
+// Detect if device is touch-enabled
+const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+
 //const taskList = document.querySelectorAll('')
 const task = document.getElementsByClassName("task");
 const startList = document.querySelector(".task-container.start"),
@@ -100,7 +103,7 @@ function createListcomp(e) {
   // indexElementFun(e.target); 
   // //(getClassName(e.target.previousElementSibling));
   // JSON.parse(localStoragegetItem("tasks") || "[]");
- 
+
   let liTag = `
   <li class="task" draggable="true" data-index=${indexElemitemc++} >
   <input type="text" class="taskInput" placeholder ="click to save">
@@ -150,10 +153,10 @@ function inputFun(event) {
   let storage = getClassName(li.parentElement);
   //(event.target);
 
-   
-  if (event.key == "Enter" ) {
+
+  if (event.key == "Enter") {
     input.disabled = true;
-    
+
     //update local storage
     // updateLocalStorage(li, storage, tasks);
     updateTaskEvent(li.parentElement, storage);
@@ -162,12 +165,10 @@ function inputFun(event) {
 }
 
 function controlFunFirst(event) {
- 
-  let input = event.target.parentElement.previousElementSibling; 
 
- 
+  let input = event.target.parentElement.previousElementSibling;
   input.disabled = false;
-   
+
 }
 function controlFunLast(event) {
 
@@ -182,18 +183,12 @@ function controlFunLast(event) {
 
 function edit(li) {
   //edit
- 
+
   let input = li.firstElementChild;
   let control = input.nextElementSibling;
-   
-  window.onclick = e =>  {
-    if(!li.contains(e.target)){
-       input.disabled = true; 
-        let storage = getClassName(li.parentElement);
-         //console.log(storage,li);
-       updateTaskEvent(li.parentElement, storage);
-    }
-   }
+  console.log('hello');
+  
+  // we use addEventListener because , for remove theme 
   input.addEventListener("keydown", inputFun);
   control.firstElementChild.addEventListener("click", controlFunFirst);
   control.lastElementChild.addEventListener('click', controlFunLast);
@@ -209,7 +204,7 @@ function updateLocalStorage(elem, storage, tasks) {
   };
 
 
-  
+
   //item.index = tasks.length;
   //elem.setAttribute('data-index',item.index);
   //tasks.push(item);
@@ -275,75 +270,147 @@ function dragOver(e) {
   e.preventDefault();
   this.classList.add("move");
 }
-function dragLeave() {
- 
-  this.classList.remove("move");
+function dragLeave(e) {
+   
 }
 let isdroped = false;
 let elementEvent;
 let elementEventParent;
-function addEventListener(task) {
+let intersectingElement ;
+let beforeIntersectingElement ; 
+function drag(e) {
+  
+}
 
-  task.addEventListener('drag', (e) => {
-    if (e.target.nodeName === 'LI')
-      elementEvent = e.target;
+function addEventListener(task) {
+if(!isTouchDevice) { 
+  task.addEventListener('dragstart',(event)=> { 
+    task.style.opacity = 0.3 ; 
+  })
+    task.addEventListener('drag', (e) => {
+    if (e.target.nodeName === 'LI') { 
+    elementEvent = e.target;
     elementEventParent = elementEvent.parentElement;
+   
+    }
+   
     ////(elementEvent);
   })
-  task.addEventListener("dragstart", e => {
-
-  });
+  
   task.addEventListener("dragover", dragOver);
-  task.addEventListener("dragleave", dragLeave);
+  task.addEventListener("dragleave", (event)=> { 
+    console.log('leave');
+    
+    task.classList.remove("move");
+    task.style.opacity = 1 ;
+    console.log(task,task.style.opacity);
+  });
   task.addEventListener("drop", (event) => {
-
+    elementEvent.style.opacity = 1 ; 
     //("event : dragDrop");
     event.target.classList.remove("move");
     //(event.target, elementEvent);
     event.target.insertAdjacentElement('beforebegin', elementEvent);
-
     //save 
     let storage;
     storage = getClassName(event.target.parentElement);
-    //(storage);
-    //('update');
-
-    // indStart++ ;   
-    //  //(indStart);
-    //  elementEvent.setAttribute('data-index',indStart);
-    // in cas of 0 
-    //delet 
-
-
-
     let storageElem = getClassName(elementEventParent);
     //('delet', storageElem);
     //let tasks2 = JSON.parse(localStorage.getItem(`${storageElem}`) || '[]');
     updateTaskEvent(elementEventParent, storageElem);
     updateTaskEvent(event.target.parentElement, storage);
-    //(event.target, elementEvent.nodeName, event.target.parentElement);
 
-    //(elementEvent, event.target);
-    //if(elementEvent.nodeName === 'LI')
-    //edit(elementEvent);
-
-    /*
-    updateLocalStorage(elementEvent,'start',tasks);
-    */
-    /*
-     // set flag variable to true after the condition is executed
-     //(elementEvent);
-     let input = elementEvent.firstElementChild;
-     let control = input.nextElementSibling;
-     //(input, control);
-     input.removeEventListener("keydown", inputFun);
-     control.firstElementChild.removeEventListener("click", controlFunFirst);
-     control.lastElementChild.removeEventListener('click', controlFunLast);
-     edit(elementEvent);
-     */
   });
 
+/* else { 
+  task.addEventListener('touchstart',f)
+} */ 
+//this code from chatgpt 
+}  else {
+  let touchX, touchY, taskX, taskY;
+   // Add touch event listeners to the task
+task.addEventListener('touchstart', (event)=> { 
+  // Save the starting touch position and task position
+ 
+  console.log(event.touches);
+  touchX = event.touches[0].clientX;
+  touchY = event.touches[0].clientY;
+  taskX = task.offsetLeft;
+  taskY = task.offsetTop;
+});
+
+task.addEventListener('touchmove', (event) => {  
+event.preventDefault();
+task.style.position = 'absolute';
+task.style.width ='250px';
+task.style.opacity = 0.3 ;
+// Calculate the new position of the task based on the touch movement
+const deltaX = event.touches[0].clientX - touchX;
+const deltaY = event.touches[0].clientY - touchY;
+const newTaskX = taskX + deltaX;
+const newTaskY = taskY + deltaY;
+
+// Update the position of the task
+//task.style.left = newTaskX + 'px';
+task.style.top = newTaskY + 'px';
+
+}) ; 
+  console.log('I am very sorry, the mobile version is coming as soon as possible (:');
+} /*
+ 
+ 
+
+  // Get the bounding rectangle of the current li element
+  const liRect = li.getBoundingClientRect();
+
+  // Calculate the coordinates of the center of the li element
+  const liCenterX = liRect.left + liRect.width / 2;
+  const liCenterY = liRect.top + liRect.height / 2;
+
+  // Get the element at the current touch point
+  const intersectingElement = document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY);
+
+  // Check if the intersecting element is an li element
+  if (intersectingElement.nodeName === 'LI') {
+    // Get the bounding rectangle of the intersecting li element
+    const intersectingRect = intersectingElement.getBoundingClientRect();
+
+    // Calculate the coordinates of the center of the intersecting li element
+    const intersectingCenterX = intersectingRect.left + intersectingRect.width / 2;
+    const intersectingCenterY = intersectingRect.top + intersectingRect.height / 2;
+
+    // Calculate the distance between the centers of the two li elements
+    const distance = Math.sqrt((liCenterX - intersectingCenterX) ** 2 + (liCenterY - intersectingCenterY) ** 2);
+
+    // If the distance is less than 5px, change the background color of the intersecting li element to red
+    if (distance < 5) {
+      intersectingElement.style.backgroundColor = 'red';
+    }
+  }
+
+  
+  
+  });
+  task.addEventListener('touchend',(event)=> { 
+     intersectingElement.classList.remove('move');   
+  
+  })
+  
+  /*
+  task.addEventListener('touchend',(event)=> { 
+    task.style.position ='relative';
+    task.style.width = '100%';
+    task.style.opacity = 1 ; 
+  })
 }
+*/ 
+}
+
+
+
+
+
+
 function updateTaskEvent(event, storage) {
 
   let taskEvent = Array.from(event.querySelectorAll('.task')).reverse();
