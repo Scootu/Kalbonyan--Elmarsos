b61c7e34bed62858446836b9a23a0b4e34fff514
scoote <bacetd2004@gmail.com>
2023-05-21 17:06:13 +0100

    finiching the section 8

--- React-The-completed-juide/section8/src/User/AddUser.js
+++ React-The-completed-juide/section8/src/User/AddUser.js
@@ -1,40 +1,36 @@
-import React ,{useState} from "react";
+import React ,{useRef} from "react";
 import Button from '../UI/Button';
 import styled from './AddUser.module.css';
 import Card from "../UI/Card";
 
 const AddUser = (props) => {
 
-const [modUserName,setUserName] = useState('');
-const [modUserAge,setUserAge] =useState('');
+const nameUser =  useRef();
+const ageUser = useRef();
 
-const addUserForm = (event) =>  { 
+const addUserForm = (event) =>  {
+  const entredName = nameUser.current.value ;
+  const entredAge = ageUser.current.value ;  
    event.preventDefault(); 
     
-   if(modUserName.trim().length === 0 || modUserAge.trim().length === 0 || +modUserAge < 1) {
+   if(entredName.trim().length === 0 || entredAge.trim().length === 0 || +entredAge < 1) {
       props.onGetAns();
       return ; 
    }
-   console.log(modUserName , modUserAge);
-   props.setGetUserData({id:Math.random().toString() ,name:modUserName , age:modUserAge});
-   setUserName('');
-   setUserAge('');
-}
-
-const userNameChangeHandler = (event) => { 
-  setUserName(event.target.value);
-} 
-const userAgeChangeHandler = (event) => { 
-  setUserAge(event.target.value);
+   console.log(entredName , entredAge);
+   props.setGetUserData({id:Math.random().toString() ,name:entredName , age:entredAge});
+   nameUser.current.value = '' ; 
+   ageUser.current.value = '' ; 
+   
 }
 
     return (
     <Card >
      <form onSubmit={addUserForm} className={styled['form-control']}>
        <label>UserName</label>
-       <input type="text" value={modUserName} onChange={userNameChangeHandler}  />
+       <input type="text" ref={nameUser} />
        <label>Age(years)</label>
-       <input type="number" value={modUserAge} onChange={userAgeChangeHandler} />
+       <input type="number" ref={ageUser}/>
       <div> 
        <Button type={'submit'}>Add User</Button>
       </div>
