3e44cab7d328c8e4666262bc94998c08b61561d9
aneshamdaoui <bacetd2004@gmail.com>
2023-07-08 21:04:40 +0100

    working in file

---
+++ React-The-completed-juide/section11/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,13 @@
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = props => { 
+   return (<button className={classes.button}> 
+    <span className={classes.icon}>
+        
+    </span>
+    <span></span>
+    <span></span>
+   </button>)
+}
+
+export default HeaderCartButton ;
