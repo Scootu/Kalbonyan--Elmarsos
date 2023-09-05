5da2a28c178844a9d615a23b361492a84cb53c45
aneshamdaoui <bacetd2004@gmail.com>
2023-09-05 23:16:51 +0100

    f

---
+++ 02-Udemy/03-vanila-web-projects/04-exchange-ratet-calculator/script.js
@@ -0,0 +1,65 @@
+const allCurrency = document.querySelectorAll(".coin-change select");
+
+// const outherCoinCurrency = document.querySelector("");
+const yourCurrency = document.getElementById("selctedYour");
+const outherCurrency = document.getElementById("selectOuther");
+
+const yourCoinValue = document.getElementById("number-your");
+const outherCoinValue = document.getElementById("number-outher");
+
+const exancheRate = document.getElementById("exchange-rate");
+
+const btnChange = document.getElementById("save");
+let changed = false;
+function showUpSelectValues(data) {
+  changed = true;
+  let optionTag = "";
+
+  for (const key in data) {
+    const option = `<option value="${key}">${key}</option>`;
+    optionTag += option;
+  }
+  yourCurrency.innerHTML = optionTag;
+  outherCurrency.innerHTML = optionTag;
+}
+const CurrencyList = async () => {
+  // const selctedCoin = selectOption.value ;
+  const coin = allCurrency[0].value.toUpperCase();
+  const outherCoin = allCurrency[1].value.toUpperCase();
+  const responce = await fetch(
+    `https://v6.exchangerate-api.com/v6/40548a90a31b3d73cf032060/latest/${coin}`
+  );
+  const data = await responce.json();
+  //base_code
+  //conversion_rates
+  const { conversion_rates } = data;
+  if (!changed) {
+    showUpSelectValues(conversion_rates);
+  }
+  // return { coin, outherCoin, base_code, conversion_rates };
+  let rateChange = 1;
+  // setCurrencyRate({ coin, outherCoin, base_code, conversion_rates });
+  for (const key in conversion_rates) {
+    if (key === outherCoin) {
+      rateChange = conversion_rates[key];
+      exancheRate.innerHTML = `1 ${coin} = ${conversion_rates[key]}`;
+    }
+  }
+
+  const valueCoinOuther = (
+    Math.round(+yourCoinValue.value * rateChange * 100) / 100
+  ).toFixed(2);
+  outherCoinValue.value = valueCoinOuther;
+  //setCurrencyRate
+};
+// function setCurrencyRate(data) {
+//   for (const key in data.conversion_rates) {
+//     if (key === data.outherCoin) {
+//       exancheRate.innerHTML = `1 ${data.coin} = ${data.conversion_rates[key]}`;
+//     }
+//   }
+// }
+
+CurrencyList();
+
+btnChange.addEventListener("click", CurrencyList);
