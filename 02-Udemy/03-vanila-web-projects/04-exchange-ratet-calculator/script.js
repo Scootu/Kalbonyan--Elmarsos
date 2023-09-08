4909580240b24e19400ab890a4a652a7392419d3
aneshamdaoui <bacetd2004@gmail.com>
2023-09-08 22:12:23 +0100

    working in  exchange-rate-calculator

--- 02-Udemy/03-vanila-web-projects/04-exchange-ratet-calculator/script.js
+++ 02-Udemy/03-vanila-web-projects/04-exchange-ratet-calculator/script.js
@@ -1,5 +1,3 @@
-const allCurrency = document.querySelectorAll(".coin-change select");
-
 // const outherCoinCurrency = document.querySelector("");
 const yourCurrency = document.getElementById("selctedYour");
 const outherCurrency = document.getElementById("selectOuther");
@@ -9,81 +7,52 @@ const exancheRate = document.getElementById("exchange-rate");
 
 const btnChange = document.getElementById("save");
 let changed = false;
-function showUpSelectValues(data) {
-  // const responce = await fetch(
-  //   `https://v6.exchangerate-api.com/v6/40548a90a31b3d73cf032060/latest/${coin}`
-  // );
-  changed = true;
-  let optionTag = "";
+let rate_fetched_data = undefined;
 
-  for (const key in data) {
-    const option = `<option value="${key}">${key}</option>`;
-    optionTag += option;
-  }
-  yourCurrency.innerHTML = optionTag;
-  outherCurrency.innerHTML = optionTag;
-}
-const CurrencyList = async () => {
+async function CurrencyList() {
   // const selctedCoin = selectOption.value ;
-  const coin = allCurrency[0].value.toUpperCase();
-  const outherCoin = allCurrency[1].value.toUpperCase();
-  // const responce = await fetch(
-  //   `https://v6.exchangerate-api.com/v6/40548a90a31b3d73cf032060/latest/${coin}`
-  // );
-  // const data = await responce.json();
-  //base_code
-  //conversion_rates
-  // const { conversion_rates } = data;
-  if (!changed) {
-    showUpSelectValues(conversion_rates);
+  const coin = yourCurrency.value.toUpperCase();
+  const responce = await fetch(
+    `https://v6.exchangerate-api.com/v6/40548a90a31b3d73cf032060/latest/${coin}`
+  );
+  const data = await responce.json();
+  if (!responce.ok) {
+    throw new Error("Coulde not fetch the data , check your internet!");
   }
-  // return { coin, outherCoin, base_code, conversion_rates };
-  let rateChange = 1;
-  // setCurrencyRate({ coin, outherCoin, base_code, conversion_rates });
-  for (const key in conversion_rates) {
-    if (key === outherCoin) {
-      rateChange = conversion_rates[key];
-      exancheRate.innerHTML = `1 ${coin} = ${conversion_rates[key]}`;
-    }
-  }
+  // base_code
+  // conversion_rates
+  const { conversion_rates } = data;
 
-  const valueCoinOuther = (
-    Math.round(+yourCoinValue.value * rateChange * 100) / 100
-  ).toFixed(2);
-
-  outherCoinValue.value = valueCoinOuther;
-  //setCurrencyRate
-};
-
-// function setCurrencyRate(data) {
-//   for (const key in data.conversion_rates) {
-//     if (key === data.outherCoin) {
-//       exancheRate.innerHTML = `1 ${data.coin} = ${data.conversion_rates[key]}`;
-//     }
-//   }
-// }
-
-function changeOutherValue() {
-  const yourCoinValue = document.getElementById("number-your");
-  const outherCoinValue = document.getElementById("number-outher");
-  const coin = allCurrency[0].value.toUpperCase();
-  //get rateChange
-  const rateChange = 0.988;
-
-  const valueCoinOuther = (
-    Math.round(+yourCoinValue.value * rateChange * 100) / 100
-  ).toFixed(2);
-  exancheRate.innerHTML = `1 ${coin} = ${rateChange}`;
-  outherCoinValue.value = valueCoinOuther;
-}
-function swapValue() {
-  let yourCurrency = document.getElementById("selctedYour");
-  let outherCurrency = document.getElementById("selectOuther");
-  yourCurrency.value = outherCoinValue;
-  outherCurrency.value = yourCoinValue;
+  rate_fetched_data = conversion_rates;
   changeOutherValue();
 }
-// CurrencyList();
-changeOutherValue();
-btnChange.addEventListener("click", swapValue);
-yourCoinValue.addEventListener("change", changeOutherValue);
+
+function changeOutherValue() {
+  const coin = yourCurrency.value.toUpperCase();
+  const outherCoin = outherCurrency.value.toUpperCase();
+
+  let rateChange = 1;
+  for (const key in rate_fetched_data) {
+    if (key === outherCoin) {
+      rateChange = rate_fetched_data[key];
+      exancheRate.innerHTML = `1 ${coin} = ${rate_fetched_data[key]}`;
+    }
+  }
+  const valueCoinOuther = (
+    Math.round(+yourCoinValue.value * rateChange * 100) / 100
+  ).toFixed(2);
+  exancheRate.innerHTML = `1 ${coin} = ${rateChange} ${outherCoin}`;
+
+  outherCoinValue.value = valueCoinOuther;
+}
+
+CurrencyList();
+btnChange.addEventListener("click", () => {
+  const temp = yourCurrency.value;
+  yourCurrency.value = outherCurrency.value;
+  outherCurrency.value = temp;
+  CurrencyList();
+});
+yourCoinValue.addEventListener("input", changeOutherValue);
+yourCurrency.addEventListener("change", CurrencyList);
+outherCurrency.addEventListener("change", CurrencyList);
