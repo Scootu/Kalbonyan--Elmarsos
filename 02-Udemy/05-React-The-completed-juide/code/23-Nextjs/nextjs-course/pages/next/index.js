06ac5f7d2d0f3f99b59eecaf21a2cfd833cf4fac
aneshamdaoui <bacetd2004@gmail.com>
2023-09-21 23:10:54 +0100

    wo

---
+++ 02-Udemy/05-React-The-completed-juide/code/23-Nextjs/nextjs-course/pages/next/index.js
@@ -0,0 +1,6 @@
+// ourdomain.com/next
+function NextPage() {
+    return <h1>Next page</h1>;
+ }
+  
+  export default NextPage;
---
+++ 02-Udemy/05-React-The-completed-juide/code/23-Nextjs/nextjs-course/pages/next/something-important.js
@@ -0,0 +1,5 @@
+function DetailPage() {
+  return <h1>Detail page</h1>;
+}
+
+export default DetailPage;
