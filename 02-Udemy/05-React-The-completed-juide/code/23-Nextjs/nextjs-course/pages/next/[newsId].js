927f4dff0a4c15d8dbd9069063a82719f086cfd5
aneshamdaoui <bacetd2004@gmail.com>
2023-09-22 22:09:53 +0100

    working in nextjs project tutorial

---
+++ 02-Udemy/05-React-The-completed-juide/code/23-Nextjs/nextjs-course/pages/next/[newsId].js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+function DetailPage() {
+  const router = useRouter();
+  const newsId = router.query.newsId ;
+  console.log(newsId);
+  
+  return <h1>Detail page</h1>;
+}
+
+export default DetailPage;
--- 02-Udemy/05-React-The-completed-juide/code/23-Nextjs/nextjs-course/pages/next/index.js
+++ 02-Udemy/05-React-The-completed-juide/code/23-Nextjs/nextjs-course/pages/next/index.js
@@ -1,6 +1,23 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 // ourdomain.com/next
 function NextPage() {
-    return <h1>Next page</h1>;
- }
-  
-  export default NextPage;
+  return (
+    <Fragment>
+      <h1>Next page</h1>;
+      <ul>
+        <li>
+          <Link href={"/next/news-nextjs-features"}>
+            The news nextjs features
+          </Link>
+        </li>
+        <li>
+          <Link href={"/next/more-in-reactjs"}>More in reactjs</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NextPage;
