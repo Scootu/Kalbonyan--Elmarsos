f450d59f801a0a05b9a43fa84fba0dfc9c2400c9
aneshamdaoui <bacetd2004@gmail.com>
2023-11-11 21:10:50 +0100

    memoraze redux

---
+++ 02-Udemy/05-React-The-completed-juide/code/25-replace-redux/redux-essentials-example/src/app/store.js
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+
