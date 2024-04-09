9945d767bbcb742c6f7f023fe2da1bee3c6da830
anesHamdaoui <bacetd2004@gmail.com>
2024-04-09 06:59:36 +0100

    deference between function constructor and function declaration

--- 02-Udemy/06-Nodejs-The-completed-juide/Code/1.Node_basics/commonJS/globalVariable.js
+++ 02-Udemy/06-Nodejs-The-completed-juide/Code/1.Node_basics/commonJS/globalVariable.js
@@ -1,11 +1,19 @@
 // Create a global property with `var`
-var x = 10;
+var x = 10; // In nodejs this is a treated as local variable not a global variable because the top-level scope in node js is note the global scope
 
 function createFunction1() {
   const x = 20;
-  return new Function("return 10;"); // this `x` refers to global `x`
+  return new Function("return x;"); // this `x` refers to global `x`
+}
+function createFunction2() {
+  const x = 20;
+  function f() {
+    return x; // this `x` refers to the local `x` above
+  }
+  return f;
 }
 
-const f1 = createFunction1();
-
+const f1 = createFunction1(); //ReferenceError: x is not defined
+const f2 = createFunction2();
 console.log(f1());
+console.log(f2());
